243a71496b89e06e5c2c65c84a3059fc5890243d
Hiroshi Saito <h_saito@colopl.co.jp>
2019-02-11 19:59:34 +0900

    Add interactive

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,7 @@ class MyApp extends StatelessWidget {
               )
             ],
           )),
-          Icon(Icons.star, color: Colors.red[500]),
-          Text('41')
+          FavoriteWidget(),
         ],
       ),
     );
@@ -101,5 +100,50 @@ class MyApp extends StatelessWidget {
             ),
           )
         ]);
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+  int _favoriteCount = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _isFavorited = false;
+        _favoriteCount--;
+      } else {
+        _isFavorited = true;
+        _favoriteCount++;
+      }
+    });
   }
 }
